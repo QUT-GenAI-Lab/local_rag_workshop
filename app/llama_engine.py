c88c3b7290e25408f38439526b7c4f7850acef73
WillSH97 <williamsh1997@hotmail.com>
2024-12-16 11:33:46 +1000

    minor updates to verbosity, cleaning

--- app/llama_engine.py
+++ app/llama_engine.py
@@ -1,6 +1,7 @@
 from llama_cpp import Llama
 
-llm = Llama(model_path = 'Llama-3.2-3B-Instruct-abliterated.Q4_K_S.gguf', n_ctx=2048) #set context window HERE! Then use external logic for setting 
+llm = Llama(model_path = 'models/Llama-3.2-3B-Instruct-abliterated.Q4_K_S.gguf', n_ctx=2048) #set context window HERE! Then use external logic for setting processed context window idk????
+
 
 ### ^ also potentially use this for model swap???? Unsure if that's a feature I want people to have
 
