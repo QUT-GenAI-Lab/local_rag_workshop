f5c243b37a2117da6aa74e7bb64c5a821689dfd0
WillSH97 <williamsh1997@hotmail.com>
2024-12-19 12:24:52 +1000

    :wrench: some build files for pyinstaller

--- app/run.py
+++ app/run.py
@@ -1,19 +1,41 @@
-import streamlit
-
+import os
+import sys
 import streamlit.web.cli as stcli
-import os, sys
 
+def get_resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
-def resolve_path(path):
-    resolved_path = os.path.abspath(os.path.join(os.getcwd(), path))
-    return resolved_path
+    return os.path.join(base_path, relative_path)
 
+def main():
+    # Set up paths
+    chromadb_path = get_resource_path('chromadbs')
+    models_path = get_resource_path('Models')
+    
+    # Set environment variables
+    os.environ['CHROMADB_PATH'] = chromadb_path
+    os.environ['MODELS_PATH'] = models_path
 
-if __name__ == "__main__":
+    # Determine the path to Streamlit_gui.py
+    streamlit_gui_path = get_resource_path('Streamlit_gui.py')
+
+    # Modify sys.argv to run Streamlit with specific port
     sys.argv = [
-        "streamlit",
-        "run",
-        resolve_path("streamlit_gui.py"),
-        "--global.developmentMode=false",
+        'streamlit', 
+        'run', 
+        streamlit_gui_path,
+        '--global.developmentMode','false',
+        '--server.port', '8501',
+        '--server.address', 'localhost',
     ]
+
+    # Run Streamlit
     sys.exit(stcli.main())
+
+if __name__ == '__main__':
+    main()
