345c49b20251175a9b609cc83431295c1188af67
WillSH97 <williamsh1997@hotmail.com>
2025-02-19 14:37:22 +1000

    :memo: add generic GNU license to source files

--- app/llama_engine.py
+++ app/llama_engine.py
@@ -1,3 +1,25 @@
+'''
+    RAGChat - a GUI for the quick development of RAG chatbots, used to
+    teach the basic intuitions behind RAG LLMs.
+    
+    Copyright (C) 2025 QUT GenAI Lab
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    For more information, contact QUT GenAI lab via: genailab@qut.edu.au
+'''
+
 import ollama
 from ollama import chat, ChatResponse
 import subprocess
